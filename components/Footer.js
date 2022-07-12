be90e80705c71b10ded51bc8239230ebbebff77a
Maximiliano Cifuentes <55811357+elcomparemaquito@users.noreply.github.com>
2022-07-12 14:42:39 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Hecho en Chile con <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
